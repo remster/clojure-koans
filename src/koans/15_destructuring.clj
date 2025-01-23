ceaa2094ba09f38953ea141b469d15bed861aae0
Remek Zajac <remek.zajac@droneup.com>
2025-01-23 17:24:33 +0100

    progress

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -35,17 +35,19 @@
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
        {:original-parts full-name :named-parts {:first first-name :last last-name}}))
 
-  ;"Break up maps by keys"
-  ;(= "123 Test Lane, Testerville, TX"
-  ;   (let [{street-address :street-address, city :city, state :state} test-address]
-  ;     __))
+  "Break up maps by keys"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{street-address :street-address, city :city, state :state} test-address]
+       (str street-address ", " city ", " state)))
 
-  ;"Or more succinctly"
-  ;(= "123 Test Lane, Testerville, TX"
-  ;   (let [{:keys [street-address __ __]} test-address]
-  ;     __))
+  "Or more succinctly"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{:keys [street-address city state]} test-address]
+       (str street-address ", " city ", " state)))
 
-  ;"All together now!"
-  ;(= "Test Testerson, 123 Test Lane, Testerville, TX"
-  ;   (___ ["Test" "Testerson"] test-address))
+  "All together now!"
+  (= "Test Testerson, 123 Test Lane, Testerville, TX"
+     ((fn [[first-name last-name] {:keys [street-address city state]}]
+        (str first-name " " last-name ", " street-address ", " city ", " state))
+      ["Test" "Testerson"] test-address))
 )
